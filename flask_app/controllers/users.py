53ca65c172b0adb62ffc472516cf72cf3e19260d
Robert Godlewski <robtechg5@gmail.com>
2022-05-24 20:20:57 -0700

    Updated controllers and added some pages.

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,18 +1,53 @@
 from flask import render_template, redirect, session, request, flash
 from flask_app import app
-#from flask_app.models import user
-#from flask_bcrypt import Bcrypt
+from flask_app.models import user
+from flask_bcrypt import Bcrypt
 
-#bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 
 
-@app.route('/')
-def test():
-    # Note to group - Just made a temporary home page layout to test out HTML iframe tag.
-    # Will probably need a search all posts here by newest on top in the models for the home page.
-    # for the api to work we need to review https://developers.google.com/youtube/iframe_api_reference
-    # Might be better to move to post for the base route since we will need all of the posts on the main page without any user.
-    temp_data = {
-        "link": "https://www.youtube.com/watch?v=uwyRxK0htpI"
+@app.route('/login')
+def login(): 
+    return render_template("login_page.html")
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    if not user.User.validate_user(request.form):
+        return redirect('/login')
+    data = {
+        'user_name': request.form['user_name'],
+        'email': request.form['email'],
+        'password': bcrypt.generate_password_hash(request.form['password'])
     }
-    return render_template("index.html", posts=temp_data)
+    user_id = user.User.register_user(data)
+    session['user_id'] = user_id
+    return redirect('/dashboard')
+
+
+@app.route('/login_user', methods=['POST'])
+def login_user():
+    user_in_db = user.User.user_by_email(request.form)
+    if not user_in_db:
+        flash("Invalid Email", "login")
+        return redirect('/login')
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+        flash("Invalid Password", "login")
+        return redirect('/login')
+    session['user_id'] = user_in_db.id
+    return redirect('/dashboard')
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')
+    user_id = session['user_id']
+    user_info = user.User.user_by_id({"id": user_id})
+    return render_template("dashboard.html", user=user_info)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
