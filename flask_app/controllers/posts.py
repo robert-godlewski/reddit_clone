0a775824539c30fc6555b4d83291a2d303a5a309
Nathaniel Ramirez <nramire01@gmail.com>
2022-05-26 01:00:23 -0500

    changed phrasing to full post, added ability to add comments on post, added the comments on posts, broke the perfect CSS

--- flask_app/controllers/posts.py
+++ flask_app/controllers/posts.py
@@ -1,22 +1,23 @@
+from pymysql import NULL
 from flask import render_template, redirect, session, request, flash
 from flask_app import app
 from flask_app.models import comment, group, post, user
-#from flask_bcrypt import Bcrypt
+# from flask_bcrypt import Bcrypt
 
-#bcrypt = Bcrypt(app)
+# bcrypt = Bcrypt(app)
 
 
 # for the api to work we need to review https://developers.google.com/youtube/iframe_api_reference
 @app.route('/')
 def index():
     all_posts = post.Post.get_all_posts()
-    #print('All of the posts in route:')
-    #print(all_posts)
+    # print('All of the posts in route:')
+    # print(all_posts)
     return render_template("index.html", posts=all_posts)
 
 
 @app.route('/create_post', methods=['POST'])
-def create_post(): 
+def create_post():
     if 'user_id' not in session:
         return redirect('/logout')
     if not post.Post.validate_post(request.form):
@@ -32,13 +33,23 @@ def create_post():
 
 @app.route('/edit_post/<int:id>')
 def edit_post(id):
-    if 'user_id' not in session:
-        return redirect('/logout')
     post_info = post.Post.show_one_post({"id": id})
     return render_template("edit_post.html", post=post_info)
 
 
-@app.route('/update_post', methods=['POST'])
+@app.route('/g/groupnamehere/<int:id>')
+def focus_post(id):
+    all_posts = post.Post.focus_post({"id": id})
+    list = []
+    for all in all_posts:
+        list.append(all)
+    if 'user_id' not in session:
+        return render_template("focus_post.html", all_posts=list)
+    user_id = session['user_id']
+    return render_template("focus_post.html", all_posts=list, user_id=user_id)
+
+
+@ app.route('/update_post', methods=['POST'])
 def update_post():
     if 'user_id' not in session:
         return redirect('/logout')
